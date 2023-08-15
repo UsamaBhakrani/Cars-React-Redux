026320d6644cd55ddfa45f174b5bb74a17a41846
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-15 16:38:17 +0500

    CarSearch Structure

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
 
 const CarSearch = () => {
-  return (
-    <div>CarSearch</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default CarSearch
+  const searchTerm = useSelector((state) => {
+    return state.cars.searchTerm;
+  });
+
+  const handleSearchTerm = (e) => {
+    dispatch(changeSearchTerm(e.target.value));
+  };
+
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field-is-horizontal">
+        <label className="label">Search</label>
+        <input
+          className="input"
+          value={searchTerm}
+          onChange={handleSearchTerm}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CarSearch;
